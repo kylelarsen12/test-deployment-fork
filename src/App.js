9b41376df2b8121492200fa7a14e40afec24a1aa
Daniel Bouless <dbouless76@gmail.com>
2022-09-12 20:31:23 -0400

    replaced app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import './App.css';
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import CatchButton from './components/CatchButton'
+import ChangeColorButton from './components/ChangeColorButton'
+import FleeButton from './components/FleeButton'
+import RemoveButton from './components/RemoveButton'
 
 function App() {
   return (
-    <div>
-     
-      
+    <div className="App">
+    <ButtonGroup>
+      <FleeButton />
+      <CatchButton />
+    </ButtonGroup>
+
+      <RemoveButton />
+      <ChangeColorButton />
     </div>
-    
   );
 }
 
