1d4099282c5e302c7f017f19e1a738118153a027
Daniel Bouless <dbouless76@gmail.com>
2022-09-12 20:27:16 -0400

    removed app.js bs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 
-import TeamCard from './components/TeamCard';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import WildPokemonCard from './components/WildPokemonCard';
-import Container from 'react-bootstrap/Container'
-import Stack from 'react-bootstrap/Stack'
-
 
 function App() {
   return (
     <div>
-      <Container>
-        <Stack gap={3}>
-          <TeamCard/>
-      <WildPokemonCard/>
-          </Stack> 
-        
-      </Container>
+     
       
     </div>
     
