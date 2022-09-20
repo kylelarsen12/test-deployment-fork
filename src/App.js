73b9700ce7b03ba492519640ce12a513c8a35fad
unknown <kylelarsen12@gmail.com>
2022-09-20 19:54:08 -0400

    Change wildpokemoncard and app.js to communicate with backend and render name/image

--- src/App.js
+++ src/App.js
@@ -1,76 +1,79 @@
-
 //import TeamStats from "./components/TeamStats";
 
-import React, { useEffect, useState } from 'react'
-import {BrowserRouter, Routes, Route } from "react-router-dom"
-import TeamStatsCard from './components/TeamStatsCard'
-import AddTeam from './pages/AddTeam'
-import Home from './pages/Home'
-import TeamStats from './pages/TeamStats'
-import PokeDex from './pages/PokeDex'
-
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TeamStatsCard from "./components/TeamStatsCard";
+import AddTeam from "./pages/AddTeam";
+import Home from "./pages/Home";
+import TeamStats from "./pages/TeamStats";
+import PokeDex from "./pages/PokeDex";
 
 function App() {
-  const [ data, setData ] = useState('')
-  const [ id, setId ] = useState(Math.floor(Math.random()*150))
-  const [ caught, setCaught ] = useState()
- 
-  const apiURL = `https://pokeapi.co/api/v2/pokemon/` + id
- 
-  
+  const [data, setData] = useState({});
+  const [id, setId] = useState(Math.floor(Math.random() * 150));
+  const [caught, setCaught] = useState();
+
+  const apiURL = `http://localhost:5050/pokemon`;
+
   //const endpoint = 'ditto'
 
-useEffect(()=>{
-  fetchData(apiURL)
-},[])
+  useEffect(() => {
+    fetchData(apiURL);
+  }, []);
 
- async function fetchData  (url) {
-    try{
-      const response = await fetch (url)// link to backend localhost:5050 yuh
+  async function fetchData(url) {
+    try {
+      const response = await fetch(url, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      }); // link to backend localhost:5050 yuh
       //    .../team   .../storedpokemon
-      const data = await response.json()    
-      setData(data)
-      console.log(url)
-      return data
-    }catch(err){
-      console.error("FUCKED")
+      const data = await response.json();
+      setData(data);
+      console.log(data);
+      console.log(url);
+      return data;
+    } catch (err) {
+      console.error("FUCKED");
     }
   }
- 
-function handleFetchData(){
-  fetchData(apiURL)
-}
 
+  function handleFetchData() {
+    fetchData(apiURL);
+  }
 
-function handleCatch(){
-  setCaught(data.id)
-  return(
-    <TeamStatsCard/>
-  )
-}
-
+  function handleCatch() {
+    setCaught(data.id);
+    return <TeamStatsCard />;
+  }
 
   return (
     <div>
       <BrowserRouter>
-<Routes>
-  <Route path="/" element={<Home data={data} handleFetchData={handleFetchData} setId={setId} handleCatch={handleCatch}/>}/>
-  <Route path="/team_stats" element={<TeamStats handleCatch={handleCatch}/>}/>
-  <Route path='/add_team' element={<AddTeam/>}/>
-  <Route path='/pokedex' element={<PokeDex/>}/>
-</Routes>
-</BrowserRouter>
-    
-    {/* <TeamCard/> */}
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Home
+                data={data}
+                handleFetchData={handleFetchData}
+                setId={setId}
+                handleCatch={handleCatch}
+              />
+            }
+          />
+          <Route
+            path="/team_stats"
+            element={<TeamStats handleCatch={handleCatch} />}
+          />
+          <Route path="/add_team" element={<AddTeam />} />
+          <Route path="/pokedex" element={<PokeDex />} />
+        </Routes>
+      </BrowserRouter>
+
+      {/* <TeamCard/> */}
     </div>
   );
 }
 
-
-
-
 export default App;
-
-
-
-
--- src/components/WildPokemonCard.js
+++ src/components/WildPokemonCard.js
@@ -1,22 +1,32 @@
-
-import FleeButton from './FleeButton'
-import CatchButton from './CatchButton'
-import Card from 'react-bootstrap/Card';
-import ButtonGroup from 'react-bootstrap/ButtonGroup'
-import Stack from 'react-bootstrap/Stack'
-
+import FleeButton from "./FleeButton";
+import CatchButton from "./CatchButton";
+import Card from "react-bootstrap/Card";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Stack from "react-bootstrap/Stack";
 
 function WildPokemonCard(props) {
   return (
-    <Card className='text-center mx-auto' border='primary' style={{ width: '30rem'}}>
-      <Card.Img variant="top" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.data.id}.png`} alt='Wild Poke Img' style={{ width: '30rem'}}/>
+    <Card
+      className="text-center mx-auto"
+      border="primary"
+      style={{ width: "30rem" }}
+    >
+      <Card.Img
+        variant="top"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.data.id}.png`}
+        alt="Wild Poke Img"
+        style={{ width: "30rem" }}
+      />
       <Card.Title>{props.data.name}</Card.Title>
       <Card.Body>
         <ButtonGroup>
-            <Stack gap={3} direction='horizontal'>
-              <FleeButton setId={props.setId} handleFetchData={props.handleFetchData}/>
-              <CatchButton handleCatch={props.handleCatch}/>
-            </Stack>
+          <Stack gap={3} direction="horizontal">
+            <FleeButton
+              setId={props.setId}
+              handleFetchData={props.handleFetchData}
+            />
+            <CatchButton handleCatch={props.handleCatch} />
+          </Stack>
         </ButtonGroup>
       </Card.Body>
     </Card>
